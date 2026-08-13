0eb4430012cc917363b6883b2860a1463fdd343d
mafilus <mafilus@localhost>
2026-08-13 17:41:55 +0200

    test: accept derived shutdown contexts

--- tests/taskhub_test.go
+++ tests/taskhub_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/mafilus/durabletask-go/backend"
 	"github.com/mafilus/durabletask-go/tests/mocks"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func Test_TaskHubWorkerStartsDependencies(t *testing.T) {
@@ -37,9 +38,9 @@ func Test_TaskHubWorkerStopsDependencies(t *testing.T) {
 	be.EXPECT().Start(ctx).Return(nil).Once()
 	orchWorker.EXPECT().Start(ctx).Return().Once()
 	actWorker.EXPECT().Start(ctx).Return().Once()
-	be.EXPECT().Stop(ctx).Return(nil).Once()
-	orchWorker.EXPECT().StopAndDrain(ctx).Return(nil).Once()
-	actWorker.EXPECT().StopAndDrain(ctx).Return(nil).Once()
+	be.EXPECT().Stop(mock.Anything).Return(nil).Once()
+	orchWorker.EXPECT().StopAndDrain(mock.Anything).Return(nil).Once()
+	actWorker.EXPECT().StopAndDrain(mock.Anything).Return(nil).Once()
 
 	w := backend.NewTaskHubWorker(be, orchWorker, actWorker, logger)
 	assert.NoError(t, w.Start(ctx))
